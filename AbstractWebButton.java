de06c958309acd67e29f8cd591dc752099503da8
Damo <damien@spatialmedia.com.au>
2021-09-24 21:23:25 +1000

    Update

--- AbstractWebButton.java
+++
@@ -1,4 +0,0 @@
-package dev.reuise.web.core;
-
-public class AbstractWebButton extends dev.reuise.core.AbstractCoreButton{
-}
---
+++ src/main/java/dev/reuise/web/core/button/AbstractWebButton.java
@@ -0,0 +1,4 @@
+package dev.reuise.web.core.button;
+
+public class AbstractWebButton extends dev.reuise.core.button.AbstractCoreButton{
+}
